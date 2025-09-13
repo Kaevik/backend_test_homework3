a309afc9392110bcb1b86e800db8898727623595
Петр <mr.kaevik@gmail.com>
2025-09-13 20:47:23 +0300

    готов

--- sql_queries.py
+++ sql_queries.py
@@ -2,31 +2,32 @@
 # Duration - разница между scheduled_arrival и scheduled_departure.
 # В ответе должно быть 2 колонки [flight_no, duration]
 TASK_1_QUERY = """
+SELECT flight_no,
+       scheduled_arrival - scheduled_departure AS duration
+FROM flights
+ORDER BY duration
+LIMIT 5;
 """
-#  flight_no | duration
-# -----------+----------
-#  PG0235    | 00:25:00
-#  PG0234    | 00:25:00
-#  PG0233    | 00:25:00
-#  PG0235    | 00:25:00
-#  PG0234    | 00:25:00
-
 
 # Вывести топ 3 рейса по числу упоминаний в таблице flights
 # количество упоминаний которых меньше 50
 # В ответе должно быть 2 колонки [flight_no, count]
 TASK_2_QUERY = """
+SELECT flight_no,
+       COUNT(*) AS count
+FROM flights
+GROUP BY flight_no
+HAVING COUNT(*) < 50
+ORDER BY count DESC
+LIMIT 3;
 """
-#  flight_no | count
-# -----------+-------
-#  PG0260    |    27
-#  PG0371    |    27
-#  PG0310    |    27
 
 # Вывести число перелетов внутри одной таймзоны
 # Нужно вывести 1 значение в колонке count
 TASK_3_QUERY = """
+SELECT COUNT(*) AS count
+FROM flights f
+JOIN airports a_dep ON f.departure_airport = a_dep.airport_code
+JOIN airports a_arr ON f.arrival_airport = a_arr.airport_code
+WHERE a_dep.timezone = a_arr.timezone;
 """
-#  count
-# --------
-#  16824
